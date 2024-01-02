d10befbaca8922c8d5cf51dda3f2eb3a89de14f8
Shigatsu <shigatsu970704@gmail.com>
2024-01-03 04:05:48 +0900

    :sparkles: Feat: Scene, Camera 등록

---
+++ src/js/scripts.js
@@ -0,0 +1,46 @@
+import * as THREE from "three";
+
+// WEBGL 렌더러 인스턴스 생성
+// Three.js가 웹페이지에 공간을 할당하는데 사용하는 도구로 생각하면 됨
+const renderer = new THREE.WebGLRenderer();
+
+// 렌더러의 크기 설정
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+// 렌더러를 웹페이지에 추가
+document.body.appendChild(renderer.domElement);
+
+// 장소 생성
+const scene = new THREE.Scene();
+
+// 카메라 생성
+const camera = new THREE.PerspectiveCamera(
+    75, // 시야각
+    window.innerWidth / window.innerHeight, // 종횡비
+    0.1, // 렌더링 시작 거리
+    1000 // 렌더링 끝 거리
+);
+
+// 축 도우미 생성
+const axesHelper = new THREE.AxesHelper(5);
+scene.add(axesHelper);
+
+// 카메라의 x, y, z 좌표 설정
+camera.position.set(0, 2, 5);
+
+// 박스 추가
+const boxGeometry = new THREE.BoxGeometry();
+const boxMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
+scene.add(boxMesh);
+
+function animate(time) {
+    // 박스 회전
+    boxMesh.rotation.x = time / 1000;
+    boxMesh.rotation.y = time / 1000;
+    renderer.render(scene, camera);
+}
+
+// 장면과 카메라를 렌더러에 추가
+// renderer.render(scene, camera);
+renderer.setAnimationLoop(animate);
