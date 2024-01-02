6170743988a72c6b7d55ba68a8db906333529d5e
Shigatsu <shigatsu970704@gmail.com>
2024-01-03 04:39:01 +0900

    :sparkles: Feat: 도우미, 요소 추가

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
 // WEBGL 렌더러 인스턴스 생성
 // Three.js가 웹페이지에 공간을 할당하는데 사용하는 도구로 생각하면 됨
@@ -21,18 +22,49 @@ const camera = new THREE.PerspectiveCamera(
     1000 // 렌더링 끝 거리
 );
 
+// 카메라 컨트롤러 생성
+const orbit = new OrbitControls(camera, renderer.domElement);
+
 // 축 도우미 생성
 const axesHelper = new THREE.AxesHelper(5);
 scene.add(axesHelper);
 
+// 그리드 도우미 생성
+const gridHelper = new THREE.GridHelper(30);
+scene.add(gridHelper);
+
 // 카메라의 x, y, z 좌표 설정
-camera.position.set(0, 2, 5);
+camera.position.set(-10, 30, 30);
+// 카메라가 마우스 움직임에 반응하도록 설정
+orbit.update();
 
 // 박스 추가
-const boxGeometry = new THREE.BoxGeometry();
-const boxMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
+const boxGeometry = new THREE.BoxGeometry(); // 상자를 만듬
+const boxMaterial = new THREE.MeshBasicMaterial({
+    color: 0x00ff00,
+}); // 상자의 스킨을 만듬
+const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial); // 상자에 스킨을 입힘
 scene.add(boxMesh);
+
+// 평면 추가
+const planeGeometry = new THREE.PlaneGeometry(30, 30);
+const planeMaterial = new THREE.MeshBasicMaterial({
+    color: 0xffffff,
+    side: THREE.DoubleSide,
+});
+const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
+scene.add(planeMesh);
+planeMesh.rotation.x = -0.5 * Math.PI;
+
+// 구 추가
+const sphereGeometry = new THREE.SphereGeometry(4, 50, 50);
+const sphereMaterial = new THREE.MeshBasicMaterial({
+    color: 0x0000ff,
+    wireframe: false,
+});
+const sphereMesh = new THREE.Mesh(sphereGeometry, sphereMaterial);
+scene.add(sphereMesh);
+sphereMesh.position.set(-10, 10, 0);
 
 function animate(time) {
     // 박스 회전
