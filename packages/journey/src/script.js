a2de2d4aba67c4787e4bc1feb7a738bd26bd16d1
ShigatsuEl <shigatsu970704@gmail.com>
2024-02-27 05:17:14 +0900

    :sparkles: Feat: [journey] physics start

--- packages/journey/src/script.js
+++ packages/journey/src/script.js
@@ -1,20 +1,11 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import GUI from "lil-gui";
-import gsap from "gsap";
 
 /**
  * Debug
  */
 const gui = new GUI();
-
-const parameters = {
-    materialColor: "#ffeded",
-};
-
-gui.addColor(parameters, "materialColor").onChange(() => {
-    material.color.set(parameters.materialColor);
-    particlesMaterial.color.set(parameters.materialColor);
-});
 
 /**
  * Base
@@ -26,78 +17,69 @@ const canvas = document.querySelector("canvas.webgl");
 const scene = new THREE.Scene();
 
 /**
- * Objects
+ * Textures
  */
-// Texture
 const textureLoader = new THREE.TextureLoader();
-const gradientTexture = textureLoader.load("textures/gradients/3.jpg");
-gradientTexture.magFilter = THREE.NearestFilter;
+const cubeTextureLoader = new THREE.CubeTextureLoader();
 
-// Material
-const material = new THREE.MeshToonMaterial({
-    color: parameters.materialColor,
-    gradientMap: gradientTexture,
-});
+const environmentMapTexture = cubeTextureLoader.load([
+    "/textures/environmentMaps/0/px.png",
+    "/textures/environmentMaps/0/nx.png",
+    "/textures/environmentMaps/0/py.png",
+    "/textures/environmentMaps/0/ny.png",
+    "/textures/environmentMaps/0/pz.png",
+    "/textures/environmentMaps/0/nz.png",
+]);
 
-// Objects
-const objectsDistance = 4;
-const mesh1 = new THREE.Mesh(new THREE.TorusGeometry(1, 0.4, 16, 60), material);
-const mesh2 = new THREE.Mesh(new THREE.ConeGeometry(1, 2, 32), material);
-const mesh3 = new THREE.Mesh(
-    new THREE.TorusKnotGeometry(0.8, 0.35, 100, 16),
-    material
+/**
+ * Test sphere
+ */
+const sphere = new THREE.Mesh(
+    new THREE.SphereGeometry(0.5, 32, 32),
+    new THREE.MeshStandardMaterial({
+        metalness: 0.3,
+        roughness: 0.4,
+        envMap: environmentMapTexture,
+        envMapIntensity: 0.5,
+    })
 );
+sphere.castShadow = true;
+sphere.position.y = 0.5;
+scene.add(sphere);
 
-mesh1.position.x = 2;
-mesh2.position.x = -2;
-mesh3.position.x = 2;
-
-mesh1.position.y = -objectsDistance * 0;
-mesh2.position.y = -objectsDistance * 1;
-mesh3.position.y = -objectsDistance * 2;
-
-scene.add(mesh1, mesh2, mesh3);
-
-const sectionMeshes = [mesh1, mesh2, mesh3];
+/**
+ * Floor
+ */
+const floor = new THREE.Mesh(
+    new THREE.PlaneGeometry(10, 10),
+    new THREE.MeshStandardMaterial({
+        color: "#777777",
+        metalness: 0.3,
+        roughness: 0.4,
+        envMap: environmentMapTexture,
+        envMapIntensity: 0.5,
+    })
+);
+floor.receiveShadow = true;
+floor.rotation.x = -Math.PI * 0.5;
+scene.add(floor);
 
 /**
  * Lights
  */
-const directionalLight = new THREE.DirectionalLight("#ffffff", 3);
-directionalLight.position.set(1, 1, 0);
+const ambientLight = new THREE.AmbientLight(0xffffff, 2.1);
+scene.add(ambientLight);
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.6);
+directionalLight.castShadow = true;
+directionalLight.shadow.mapSize.set(1024, 1024);
+directionalLight.shadow.camera.far = 15;
+directionalLight.shadow.camera.left = -7;
+directionalLight.shadow.camera.top = 7;
+directionalLight.shadow.camera.right = 7;
+directionalLight.shadow.camera.bottom = -7;
+directionalLight.position.set(5, 5, 5);
 scene.add(directionalLight);
-
-/**
- * Particles
- */
-// Geometry
-const particlesCount = 200;
-const positions = new Float32Array(particlesCount * 3);
-
-for (let i = 0; i < particlesCount; i++) {
-    positions[i * 3 + 0] = (Math.random() - 0.5) * 10;
-    positions[i * 3 + 1] =
-        objectsDistance * 0.5 -
-        Math.random() * objectsDistance * sectionMeshes.length;
-    positions[i * 3 + 2] = (Math.random() - 0.5) * 10;
-}
-
-const particlesGeometry = new THREE.BufferGeometry();
-particlesGeometry.setAttribute(
-    "position",
-    new THREE.BufferAttribute(positions, 3)
-);
-
-// Material
-const particlesMaterial = new THREE.PointsMaterial({
-    color: parameters.materialColor,
-    sizeAttenuation: true,
-    size: 0.03,
-});
-
-// Points
-const particles = new THREE.Points(particlesGeometry, particlesMaterial);
-scene.add(particles);
 
 /**
  * Sizes
@@ -124,91 +106,41 @@ window.addEventListener("resize", () => {
 /**
  * Camera
  */
-// Group
-const cameraGroup = new THREE.Group();
-scene.add(cameraGroup);
-
 // Base camera
 const camera = new THREE.PerspectiveCamera(
-    35,
+    75,
     sizes.width / sizes.height,
     0.1,
     100
 );
-camera.position.z = 6;
-cameraGroup.add(camera);
+camera.position.set(-3, 3, 3);
+scene.add(camera);
+
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas,
-    alpha: true,
 });
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-
-/**
- * Scroll
- */
-let scrollY = window.scrollY;
-let currentSection = 0;
-
-window.addEventListener("scroll", () => {
-    scrollY = window.scrollY;
-    const newSection = Math.round(scrollY / sizes.height);
-
-    if (newSection != currentSection) {
-        currentSection = newSection;
-
-        gsap.to(sectionMeshes[currentSection].rotation, {
-            duration: 1.5,
-            ease: "power2.inOut",
-            x: "+=6",
-            y: "+=3",
-            z: "+=1.5",
-        });
-    }
-});
-
-/**
- * Cursor
- */
-const cursor = {};
-cursor.x = 0;
-cursor.y = 0;
-
-window.addEventListener("mousemove", (event) => {
-    cursor.x = event.clientX / sizes.width - 0.5;
-    cursor.y = event.clientY / sizes.height - 0.5;
-});
 
 /**
  * Animate
  */
 const clock = new THREE.Clock();
-let previousTime = 0;
 
 const tick = () => {
     const elapsedTime = clock.getElapsedTime();
-    const deltaTime = elapsedTime - previousTime;
-    previousTime = elapsedTime;
 
-    // Animate camera
-    camera.position.y = (-scrollY / sizes.height) * objectsDistance;
-
-    const parallaxX = cursor.x * 0.5;
-    const parallaxY = -cursor.y * 0.5;
-    cameraGroup.position.x +=
-        (parallaxX - cameraGroup.position.x) * 5 * deltaTime;
-    cameraGroup.position.y +=
-        (parallaxY - cameraGroup.position.y) * 5 * deltaTime;
-
-    // Animate meshes
-    for (const mesh of sectionMeshes) {
-        mesh.rotation.x += deltaTime * 0.1;
-        mesh.rotation.y += deltaTime * 0.12;
-    }
+    // Update controls
+    controls.update();
 
     // Render
     renderer.render(scene, camera);
